70b91a90f75c811cad7a4bd1f5cec50554e98668
Mihir Karbelkar <karbelkar.mihir@gmail.com>
2025-05-03 14:51:35 +0000

    middleware check

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,11 +6,6 @@ export const onRequest: MiddlewareHandler = async (context, next) => {
   const { request, url } = context;
   const hostname = url.host;
 
-  // Handle main domain
-  if (hostname === HOSTNAME) {
-    return next();
-  }
-
   // Handle subdomains
   const subdomain = hostname.split('.')[0];
 
@@ -18,6 +13,11 @@ export const onRequest: MiddlewareHandler = async (context, next) => {
   const rootHost = HOSTNAME.slice(HOSTNAME.indexOf('.') + 1)
   const newUrl = `${url.protocol}//${rootHost}/${subdomain}`
   console.log(url, hostname, HOSTNAME, newUrl, 'here')
+  // Handle main domain
+  if (hostname === HOSTNAME) {
+    return next();
+  }
+
   // Rewrite any subdomain to its corresponding path
   return context.rewrite(newUrl);
 }; 
